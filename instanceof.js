4413c71c7552f7c6f63e5628b388cfee45a60144
ZERO-DG <2583445317@qq.com>
2021-01-30 19:02:41 +0800

    实现一个instanceof

---
+++ instanceof.js
@@ -0,0 +1,22 @@
+
+//实现思路：逐层往上查找原型，如果最终的原型为 null ，证明不存在原型链中，否则存在。
+
+const myInstanceof = (left, right) => {
+    if (typeof left !== 'object' || left == null) return false; //排除基本类型
+
+    let proto = Object.getPrototypeOf(left);
+    while (true) {
+        if (proto === right.prototype) return true; //认祖归宗
+        if (proto === null) return false; //探索尽头皆是空
+        proto = Object.getPrototypeOf(proto); //找爹
+    }
+}
+
+class PerSon {
+    #name = "wxd"; //私有属性
+    constructor() { }
+}
+
+const xw = new PerSon();
+
+console.log(myInstanceof({}, PerSon));
